99f7aa07facce9cba00fa78d44d6ff3c87225eac
381706-1-DenisovVladislavL <vlad.roix@yandex.ru>
2019-10-29 21:36:35 +0300

    Added one test

--- modules/task_2/denisov_v_matrix_mult_horiz_schem_only_A/main.cpp
+++ modules/task_2/denisov_v_matrix_mult_horiz_schem_only_A/main.cpp
@@ -4,26 +4,21 @@
 #include <vector>
 #include "./matrix_mult_horiz_schem_only_A.h"
 
-// TEST(matrix_mult_horiz_schem_only_A, test_1) {
-//    int rank;
-//    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-//
-//    int sizeSide = 3;
-//    std::vector<int> matrixA = createRandomMatrix(sizeSide);
-//    std::vector<int> matrixB = createRandomMatrix(sizeSide);
-//
-//    std::vector<int> resultPar = getMatrixMultPar(matrixA, matrixB, sizeSide);
-//
-//    if (rank == 0) {
-//
-//        std::vector<int> resultSeq = getMatrixMultSeq(matrixA, matrixB, sizeSide);
-//        printMatrix(matrixA, sizeSide);
-//        printMatrix(matrixB, sizeSide);
-//        printMatrix(resultPar, sizeSide);
-//        printMatrix(resultSeq, sizeSide);
-//        ASSERT_EQ(seqCount, count);
-//    }
-// }
+ TEST(matrix_mult_horiz_schem_only_A, test_1) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    int sizeSide = 10;
+    std::vector<int> matrixA = createRandomMatrix(sizeSide);
+    std::vector<int> matrixB = createRandomMatrix(sizeSide);
+
+    std::vector<int> resultPar = getMatrixMultPar(matrixA, matrixB, sizeSide);
+
+    if (rank == 0) {
+        std::vector<int> resultSeq = getMatrixMultSeq(matrixA, matrixB, sizeSide);
+        ASSERT_EQ(resultSeq, resultPar);
+    }
+ }
 
 // TEST(matrix_mult_horiz_schem_only_A, test_print) {
 //    int rank;
