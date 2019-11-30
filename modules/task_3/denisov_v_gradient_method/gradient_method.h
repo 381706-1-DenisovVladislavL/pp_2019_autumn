034173e6a54896edb21a99da5c92a9402c96e81c
381706-1-DenisovVladislavL <vlad.roix@yandex.ru>
2019-11-30 18:20:55 +0300

    code style

--- modules/task_3/denisov_v_gradient_method/gradient_method.h
+++ modules/task_3/denisov_v_gradient_method/gradient_method.h
@@ -9,16 +9,16 @@ std::vector<double> createRandomMatrix(int sizeSide);
 std::vector<double> createRandomVector(int size);
 
 double vectorMult(const std::vector<double>& vectorA, const std::vector<double>& vectorB);
-void matrixVectorMult(const std::vector<double>& matrix, const std::vector<double>& vector, 
+void matrixVectorMult(const std::vector<double>& matrix, const std::vector<double>& vector,
     std::vector<double>& result);
 
-std::vector<double> getSolveSeq(std::vector<double> matrix, std::vector<double> vector, 
+std::vector<double> getSolveSeq(std::vector<double> matrix, std::vector<double> vector,
     int sizeSide);
 // std::vector<int> getSolvePar(std::vector<double> matrixA, std::vector<double> vectorB, 
 //    int sizeSide);
 
-void printMatrix(std::vector<double>& matrix, int size);
-void printVector(std::vector<double>& vector, int size);
-void printSystem(std::vector<double>& matrix, std::vector<double>& vector, int size);
+void printMatrix(const std::vector<double>& matrix, int size);
+void printVector(const std::vector<double>& vector, int size);
+void printSystem(const std::vector<double>& matrix, const std::vector<double>& vector, int size);
 
 #endif  // MODULES_TASK_3_DENISOV_V_GRADIENT_METHOD_GRADIENT_METHOD_H_
